24309777028c54c0b70ee292304b2c57612df97a
hulk005 <229472160@qq.com>
2018-11-27 21:08:58 +0800

    20181127

---
+++ 练习/云27/js/style/setStyle.js
@@ -0,0 +1,11 @@
+const section = document.getElementById('section');
+const head = document.getElementById('head');
+let iH = window.innerHeight;
+const headH = head.offsetHeight;
+section.style.height = iH - headH + 'px';
+
+
+window.onresize =  function(){
+    iH = window.innerHeight;
+    section.style.height = iH - headH + 'px';
+}
